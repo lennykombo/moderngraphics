0493bef0b9e8d7bfd3c461b0d901bcddb624b2ba
lennykombo <kombolenny1@gmail.com>
2026-09-10 11:39:04 +0300

    Add debug logging for Firebase Admin env vars

--- netlify/functions/sitemap.js
+++ netlify/functions/sitemap.js
@@ -11,12 +11,21 @@ function slugify(text) {
     .replace(/-+/g, "-");
 }
 
+console.log("[sitemap debug]", {
+  hasProjectId: !!process.env.FIREBASE_PROJECT_ID,
+  hasClientEmail: !!process.env.FIREBASE_CLIENT_EMAIL,
+  hasPrivateKey: !!process.env.FIREBASE_PRIVATE_KEY,
+  privateKeyLength: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.length
+    : 0,
+});
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
     }),
   });
 }
